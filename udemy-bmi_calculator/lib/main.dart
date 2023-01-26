6a9c8a110d338a1af49302c604c06220413975f2
Ali-2001-Eg <AliBusiness2001@outlook.com>
2023-01-26 16:10:45 +0200

    Initial commit

---
+++ udemy-bmi_calculator/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:udemy_flutter/bmi_screen.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+// Stateless
+// Stateful
+
+// class MyApp
+
+class MyApp extends StatelessWidget
+{
+  // constructor
+  // build
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BmiScreen(),
+    );
+  }
+}
